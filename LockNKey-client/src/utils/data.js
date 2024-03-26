91a3f5a2e9f17f9480c23e2f9e3e611d966c2cfe
Ameya02 <65841021+Ameya02@users.noreply.github.com>
2024-03-26 18:54:19 +0530

    Added for deployment

--- LockNKey-client/src/utils/data.js
+++ LockNKey-client/src/utils/data.js
@@ -3,6 +3,7 @@ import Img2 from "../assets/img2.jpg";
 import Img3 from "../assets/img3.jpg";
 import Img4 from "../assets/img4.jpg";
 
-const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:3001';
+const API_URL = import.meta.env.VITE_API_URL;
+console.log(API_URL);
 
 export {Img1, Img2,Img3,Img4, API_URL} 
